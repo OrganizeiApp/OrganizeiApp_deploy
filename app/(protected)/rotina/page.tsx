0e7af56f6a6f8c766761f898604171c610dc56bc
OrganizeiApp <neurobuddy.organizei@gmail.com>
2024-08-25 23:33:20 -0300

    commit corrigir page rotina

--- app/(protected)/rotina/page.tsx
+++ app/(protected)/rotina/page.tsx
@@ -6,27 +6,24 @@ import { MobileSidebar } from "@/components/ui/mobile-sidebar";
 import { Infos } from "../inicio/_components/info";
 
 const rotina = async () => {
-
-
     return (
-        <div className="w-full flex flex-row-reverse">
+        <div className="w-full flex flex-col lg:flex-row-reverse gap-4 lg:gap-[48px]">
             <FeedWrapper>
-            <div className="lg:hidden">
-            <MobileSidebar />
-            </div>
-            <div className='px-4 pt-4'>
-            <Infos />
-            </div>
-            <div className="lg:px-2 px-4 ">
-            <Info />
-            </div>
-            <Separator className="lg:hidden"/>
-            <div className="lg:px-6 px-4">
-            <BoardList />
-            </div>
+                <div className="lg:hidden">
+                    <MobileSidebar />
+                </div>
+                <div className='px-4 pt-4'>
+                    <Infos />
+                </div>
+                <div className="lg:px-2 px-4">
+                    <Info />
+                </div>
+                <Separator className="lg:hidden"/>
+                <div className="lg:px-6 px-4">
+                    <BoardList />
+                </div>
             </FeedWrapper>
         </div>
-        
     );
 }
 
