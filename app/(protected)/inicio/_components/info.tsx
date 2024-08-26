3cefc83ed6103d4a1955b313e94b5a3f7aa0b8d3
OrganizeiApp <neurobuddy.organizei@gmail.com>
2024-08-25 23:29:12 -0300

    info corrigido rotina

--- app/(protected)/inicio/_components/info.tsx
+++ app/(protected)/inicio/_components/info.tsx
@@ -6,7 +6,7 @@ export const Infos = async () => {
     if (!session || !session.user?.id) {
         return (
             <div className="flex items-center gap-x-4">
-                <div className="lg:pt-6 lg:pl-2 pl-4 w-full h-[60px] relative">
+                <div className="lg:pt-6 lg:pl-2 w-full h-[60px] relative">
                     <h2 className="font-extrabold lg:text-3xl text-3xl text-red-600">Sem autorização</h2>
                 </div>
             </div>
@@ -17,7 +17,7 @@ export const Infos = async () => {
 
     return (
         <div className="flex items-center gap-x-4">
-            <div className="lg:pt-6 lg:pl-2 w-full pl-4 h-[60px] relative">
+            <div className="lg:pt-6 lg:pl-2 w-full h-[60px] relative">
                 <h2 className="font-extrabold lg:text-3xl text-3xl text-[#7935E8]">Olá, {session.user.name}</h2>
             </div>
         </div>
