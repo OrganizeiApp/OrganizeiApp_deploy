98bc41876c61c2956b52483c4c324f903c707528
OrganizeiApp <neurobuddy.organizei@gmail.com>
2024-08-29 23:51:00 -0300

    correção botão termos

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     return (
         <div className="flex items-center w-full px-6 z-50 dark:bg-[#6F73D2]">
             <Logo />
-            <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+            <div className="md:ml-auto w-full justify-end flex items-center gap-x-2 text-muted-foreground">
             <Link
             href="/terms-of-service"
             >
