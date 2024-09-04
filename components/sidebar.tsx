b177b30a735f21fb48ec3a3bbcea9642805b8b2a
OrganizeiApp <neurobuddy.organizei@gmail.com>
2024-09-04 17:56:34 -0300

    esconder estante

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -37,11 +37,11 @@ export const Sidebar = ({ className }: Props) => {
             href="/rotina"
             iconSrc="/CALENDARIO.svg"
             />
-            <SidebarItem
+            {/* <SidebarItem
             label="ESTANTE"
             href="/estante"
             iconSrc="/EXTANTE.svg"
-            />
+            /> */}
             <SidebarItem
             label="FEEDBACK" 
             href="https://forms.gle/vhn5N7v77xXMqnVaA"
